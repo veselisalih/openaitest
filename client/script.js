752c1a3b8339ed28a1aab600344f306fb74d3bfd
Salih Veseli <salihveseli@gmail.com>
2022-12-26 16:06:08 -0500

    first commit

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's reponse
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-wcwr.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
